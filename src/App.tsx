5a26531e0fec57a20776f1e874941629ab4652be
Ali Alvi <ali.raza@tss.se>
2024-04-29 14:31:31 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ShadcnTest from './ShadcnTest'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <ShadcnTest />
+      </div>
+    </>
+  )
+}
+
+export default App
