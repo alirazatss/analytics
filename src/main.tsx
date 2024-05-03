cc4017a5eb6c479c32fab6f8ae02dc66dd845d84
Ali Alvi <ali.raza@tss.se>
2024-05-04 00:53:15 +0200

    updates the router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import { AppInsightsContext } from '@microsoft/applicationinsights-react-js';
@@ -11,7 +11,7 @@ import { reactPlugin } from './ApplicationInsightsService';
 
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <UsersList />,
