4ae9673aacf7b029b99ba2737f30be8ab3eb7061
Ali Alvi <ali.raza@tss.se>
2024-05-08 16:11:25 +0200

    adds the docker file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,18 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/analytics/",
+  // base: "/analytics/",
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+   },
+   server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
